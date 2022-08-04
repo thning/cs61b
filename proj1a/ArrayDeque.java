3a02ef9908c0485f333db05de915020da10db820
Ning <alexander.ning@intel.com>
2022-08-05 01:18:29 +0800

    try use resize for downsize

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,136 +1,121 @@
 public class ArrayDeque<T> {
-    private T[] items;
-    private int size; 
-    private int nextFirstIndex, nextLastIndex;
-    private double ratio;
+	private T[] items;
+	private int size; 
+	private int nextFirstIndex, nextLastIndex;
+	private double ratio;
 
-    public ArrayDeque() {
-        items = (T[]) new Object[8];
-        size = 0;
-        nextFirstIndex = 0;
-        nextLastIndex = 1;
-    }
-
-
-    // nextlast = currfirst 
-    private void resize() {
-        T[] newItems = (T[]) new Object[size * 5];
-        int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
-        for (int i = 0; i < size; i++) { 
-            if (i >= firstIndex) {
-                newItems[i - firstIndex] = items[i];
-            } else{
-                newItems[size - firstIndex + i] = items[i];
-            }
-        }
-        nextFirstIndex = newItems.length - 1;
-        nextLastIndex = size;
-        items = newItems;
-    }
+	public ArrayDeque() {
+		items = (T[]) new Object[8];
+		size = 0;
+		nextFirstIndex = 0;
+		nextLastIndex = 1;
+	}
 
 
-    private void downsize() {
-        T[] newItems = (T[]) new Object[size * 2];
-        int index = (nextFirstIndex + 1 + items.length) % items.length;
-        int count = 0;
-        while (count < size) {
-            newItems[count] = items[index];
-            index =  (index + 1 + items.length) % items.length;
-            count++;
-        }
-        nextFirstIndex = newItems.length - 1;
-        nextLastIndex = size;
-        items = newItems;
-    }
+	// nextlast = currfirst 
+	private void resize() {
+		T[] newItems = (T[]) new Object[size * 2];
+		int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
+		for (int i = 0; i < size; i++) { 
+			if (i >= firstIndex) {
+				newItems[i - firstIndex] = items[i];
+			} else{
+				newItems[size - firstIndex + i] = items[i];
+			}
+		}
+		nextFirstIndex = newItems.length - 1;
+		nextLastIndex = size;
+		items = newItems;
+	}
 
-    public void addFirst(T item) {
-        items[nextFirstIndex] = item;
-        nextFirstIndex = (nextFirstIndex - 1 + items.length) % items.length;
-        size++;
-        if (size == items.length) {
-            resize();
-        }
-    }
+	public void addFirst(T item) {
+		items[nextFirstIndex] = item;
+		nextFirstIndex = (nextFirstIndex - 1 + items.length) % items.length;
+		size++;
+		if (size == items.length) {
+			resize();
+		}
+	}
 
-    public T removeFirst() {
-        if (size == 0) {
-            return null;
-        }
-        int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
-        T removeItem = items[firstIndex];
-        items[firstIndex] = null;
-        nextFirstIndex = (nextFirstIndex + 1 + items.length) % items.length;
-        size--;
+	public T removeFirst() {
+		if (size == 0) {
+			return null;
+		}
+		int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
+		T removeItem = items[firstIndex];
+		items[firstIndex] = null;
+		nextFirstIndex = (nextFirstIndex + 1 + items.length) % items.length;
+		size--;
 
-        if ((items.length > size * 4) && (items.length >= 16)) {
-            downsize();
-        }
-        return removeItem;
-    }
+		if ((items.length > size * 4) && (items.length >= 16)) {
+			resize();
+		}
+		return removeItem;
+	}
 
-    public void addLast(T item) {
-        items[nextLastIndex] = item;
-        nextLastIndex = (nextLastIndex + 1 + items.length) % items.length;
-        size++;
-        if (size == items.length) {
-            resize();
-        }
-    }
+	public void addLast(T item) {
+		items[nextLastIndex] = item;
+		nextLastIndex = (nextLastIndex + 1 + items.length) % items.length;
+		size++;
+		if (size == items.length) {
+			resize();
+		}
+	}
 
-    public T removeLast() {
-        if (size == 0) {
-            return null;
-        }
-        int lastIndex = (nextLastIndex - 1 + items.length) % items.length;
-        T removeItem = items[lastIndex];
-        items[lastIndex] = null;
-        nextLastIndex = (nextLastIndex - 1 + items.length) % items.length;
-        size--;
-        if ((items.length > size * 4) && (items.length >= 16)) {
-            downsize();
-        }
-        return removeItem;
-    }
+	public T removeLast() {
+		if (size == 0) {
+			return null;
+		}
+		int lastIndex = (nextLastIndex - 1 + items.length) % items.length;
+		T removeItem = items[lastIndex];
+		items[lastIndex] = null;
+		nextLastIndex = (nextLastIndex - 1 + items.length) % items.length;
+		size--;
+		if ((items.length > size * 4) && (items.length >= 16)) {
+			resize();
+		}
+		return removeItem;
+	}
 
-    public int size() {
-        return size;
-    }
+	public int size() {
+		return size;
+	}
 
-    public T get(int i) {
-        if (i > size - 1) {
-            return null;
-        }
-        int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
-        int count = 0;
-        int returnIndex = firstIndex;
-        while (count != i) {
-            count++;
-            returnIndex = (returnIndex + 1 + items.length) % items.length;
-        }
-        return items[returnIndex];
-    }
+	public T get(int i) {
+		if (i > size - 1) {
+			return null;
+		}
+		int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
+		int count = 0;
+		int returnIndex = firstIndex;
+		while (count != i) {
+			count++;
+			returnIndex = (returnIndex + 1 + items.length) % items.length;
+		}
+		return items[returnIndex];
+	}
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    public void printDeque() {
-        int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
-        int count = 0;
-        int i = firstIndex;
-        while (count < size) {
-            System.out.print(items[i] + " ");
-            i = (i + 1 + items.length) % items.length;
-            count++;
-        }
-    }
+	public void printDeque() {
+		int firstIndex = (nextFirstIndex + 1 + items.length) % items.length;
+		int count = 0;
+		int i = firstIndex;
+		while (count < size) {
+			System.out.print(items[i] + " ");
+			i = (i + 1 + items.length) % items.length;
+			count++;
+		}
+	}
 
-    // public static void main(String[] args) {
-    //     ArrayDeque ArrayDeque = new ArrayDeque();
-    //     ArrayDeque.addLast(0);
- //        ArrayDeque.addLast(1);
- //        ArrayDeque.removeLast();     
- //        ArrayDeque.addLast(3);
-    // }
+	public static void main(String[] args) {
+		ArrayDeque ArrayDeque = new ArrayDeque();
+        ArrayDeque.isEmpty();
+        ArrayDeque.addFirst(1);
+        ArrayDeque.removeFirst();
+        ArrayDeque.addFirst(3);
+	}
 }
 
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,114 +1,114 @@
 public class LinkedListDeque<T> {
-    private Node sentinel;
-    private int size;
+	private Node sentinel;
+	private int size;
 
-    private class Node {
-        private Node prev;
-        private T item;
-        private Node next;
+	private class Node {
+		private Node prev;
+		private T item;
+		private Node next;
 
-        public Node(Node prev, T item, Node next) {
-            this.prev =  prev;
-            this.item = item;
-            this.next = next;
-        }
-    }
+		public Node(Node prev, T item, Node next) {
+			this.prev =  prev;
+			this.item = item;
+			this.next = next;
+		}
+	}
 
-    public LinkedListDeque() {
-        sentinel = new Node(null, null, null);
-        sentinel.prev = sentinel;
-        sentinel.next = sentinel;;
-        size = 0;
-    }
+	public LinkedListDeque() {
+		sentinel = new Node(null, null, null);
+		sentinel.prev = sentinel;
+		sentinel.next = sentinel;;
+		size = 0;
+	}
 
-    public void addFirst(T item) {
-        Node currFirstNode = sentinel.next;
-        Node newFirstNode = new Node(sentinel, item, currFirstNode);
+	public void addFirst(T item) {
+		Node currFirstNode = sentinel.next;
+		Node newFirstNode = new Node(sentinel, item, currFirstNode);
 
-        currFirstNode.prev = newFirstNode;
-        sentinel.next = newFirstNode;
-        size++;
-    }
+		currFirstNode.prev = newFirstNode;
+		sentinel.next = newFirstNode;
+		size++;
+	}
 
-    public T removeFirst() {
-        if (size == 0) {
-            return null;
-        }
-        T removeItem = sentinel.next.item;
-        Node newFirstNode = sentinel.next.next;
-        newFirstNode.prev = sentinel;
-        sentinel.next = newFirstNode;
-        size--;
-        return removeItem;
-    }
+	public T removeFirst() {
+		if (size == 0) {
+			return null;
+		}
+		T removeItem = sentinel.next.item;
+		Node newFirstNode = sentinel.next.next;
+		newFirstNode.prev = sentinel;
+		sentinel.next = newFirstNode;
+		size--;
+		return removeItem;
+	}
 
-    public void addLast(T item) { 
-        Node currLastNode = sentinel.prev;
-        Node newLastNode = new Node(sentinel.prev, item, sentinel); 
+	public void addLast(T item) { 
+		Node currLastNode = sentinel.prev;
+		Node newLastNode = new Node(sentinel.prev, item, sentinel); 
 
-        currLastNode.next = newLastNode;
-        sentinel.prev = newLastNode;
-        size++;
-    }
+		currLastNode.next = newLastNode;
+		sentinel.prev = newLastNode;
+		size++;
+	}
 
 
-    public T removeLast() {
-        if (size == 0) {
-            return null;
-        }
-        T removeItem = sentinel.prev.item;
-        Node newLastNode = sentinel.prev.prev;
-        newLastNode.next = sentinel;
-        sentinel.prev = newLastNode;
-        size--;
-        return removeItem;
-    }
+	public T removeLast() {
+		if (size == 0) {
+			return null;
+		}
+		T removeItem = sentinel.prev.item;
+		Node newLastNode = sentinel.prev.prev;
+		newLastNode.next = sentinel;
+		sentinel.prev = newLastNode;
+		size--;
+		return removeItem;
+	}
 
-    public int size() {
-        return size;
-    }
+	public int size() {
+		return size;
+	}
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
-    public T get(int index) {
-        if (index > size - 1) {
-            return null;
-        }
-        Node p = sentinel.next;
-        int i = 0;
-        while (i != index) {
-            p = p.next;
-            i++;
-        }
-        return p.item;
-    }
+	public T get(int index) {
+		if (index > size - 1) {
+			return null;
+		}
+		Node p = sentinel.next;
+		int i = 0;
+		while (i != index) {
+			p = p.next;
+			i++;
+		}
+		return p.item;
+	}
 
-    private T getRecursive(Node node, int index) {
-        if (index == 0) {
-            return node.item;
-        }
-        return getRecursive(node.next, index - 1);
-    }
+	private T getRecursive(Node node, int index) {
+		if (index == 0) {
+			return node.item;
+		}
+		return getRecursive(node.next, index - 1);
+	}
 
-    public T getRecursive(int index) {
-        return getRecursive(sentinel.next, index);
-    }
+	public T getRecursive(int index) {
+		return getRecursive(sentinel.next, index);
+	}
 
-    public void printDeque() {
-        Node p = sentinel;
-        while (p.next != sentinel) {
-            p = p.next;
-            System.out.print(p.item + " ");
-        } 
-    }
+	public void printDeque() {
+		Node p = sentinel;
+		while (p.next != sentinel) {
+			p = p.next;
+			System.out.print(p.item + " ");
+		} 
+	}
 
-    // public static void main(String[] args) {
-    //      LinkedListDeque LinkedListDeque = new LinkedListDeque();
+	// public static void main(String[] args) {
+	// 	 LinkedListDeque LinkedListDeque = new LinkedListDeque();
  //         LinkedListDeque.addLast(0);
  //         LinkedListDeque.removeLast();     
  //         LinkedListDeque.addLast(3);
  //         System.out.print(LinkedListDeque.getRecursive(0));
-    // }
+	// }
 }
